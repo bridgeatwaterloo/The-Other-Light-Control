63078f9b7c50ba102b9e08fd3179809f04628268
Machinewarlock <jameschu1874@gmail.com>
2016-07-12 16:35:29 +0100

    Moved comments above code per Ali's link

--- Machinewarlock/Blink/Blink.ino
+++ Machinewarlock/Blink/Blink.ino
@@ -1,16 +1,18 @@
 /* This sketch meets the acceptance criteria of this task https://trello.com/c/ba783WmU/2-blink-an-led */
 
+/* This is the value I have set for the pin and how it refers to the circuit board */
 int pin = 9;
 
+/* This is the setup for the pin number and what it is doing */ 
 void setup() {
   pinMode(pin,OUTPUT);
 }
-/* This is the setup for the pin number and what it is doing */ 
 
+/* This is the pin turning on and off with a delay of 500ms looping endlessly */
 void loop() {
   digitalWrite(pin,1);
   delay(500);
   digitalWrite(pin,0);
   delay(500);
 }
-/* This is the pin turning on and off with a delay of 500ms looping endlessly */
+
