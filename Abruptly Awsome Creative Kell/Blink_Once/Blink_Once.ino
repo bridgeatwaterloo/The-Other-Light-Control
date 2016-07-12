e7df80a169593ef155aa443e8e7e887b07a54c6d
Kellyana <thekellyana@gmail.com>
2016-07-12 16:30:31 +0100

    Trying a thing

---
+++ Abruptly Awsome Creative Kell/Blink_Once/Blink_Once.ino
@@ -0,0 +1,16 @@
+int pinNumber = 6; //set pin number to 6:
+
+void setup() {
+pinMode(pinNumber,OUTPUT); //set pin to output 
+}
+
+void blink() {
+ analogWrite(pinNumber, 5);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
