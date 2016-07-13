656066d08a63570a5e5558c1327309b3d6c03e24
Cristina Gavril <cris.gavril@gmail.com>
2016-07-13 11:28:24 +0100

    Cris/Blink-Fade
    some changes after Kim's suggestion

--- Cris/ledFade/ledFade.ino
+++ Cris/ledFade/ledFade.ino
@@ -7,7 +7,7 @@ int brightness = 0;
 //declare the function that will set the pin to output mode
 void setup() {
   pinMode(led, OUTPUT);
-  Serial.begin(9600);  
+  Serial.begin(9600); //the print syntax used to begin the serial monitior 
 }
 //declare a function that will run a loop 
 void loop() {
@@ -15,9 +15,10 @@ void loop() {
    brightness += fadeAmount; //changes the brightness by the fadeAmount
    if (brightness <= 0 || brightness >= 255){ //if the condition is met and the brightness is either 0 or 255
       fadeAmount =- fadeAmount; //it will change the sign for fadeAmount and depending on what it was previously, it will either decrease or increase the fadeAmount
-   Serial.print(brightness);
-   Serial.print("\n");
    }
+   Serial.print(brightness); //the print syntax used to display serial monitior details from the loop 
+   Serial.print("\n"); //this line will display each print in a new line
+   
    delay (30); 
 }
 
