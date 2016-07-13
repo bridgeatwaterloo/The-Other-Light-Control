56b97cf8c2d7f713ae005ab1c431d97ca4941c10
kimberleynyam <kimberleynyam@hotmail.co.uk>
2016-07-13 11:45:03 +0100

    updated fade

--- kimnyamhondera/fade/fade.ino
+++ kimnyamhondera/fade/fade.ino
@@ -5,15 +5,15 @@ int fadeAmount = 5; /* set initial fadeAmount */
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(led, OUTPUT);
-}
+  pinMode(led, OUTPUT); /* setting up  pin 6 to turn on */
+} 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(led, brightness);
-  brightness = brightness +fadeAmount;
+  analogWrite(led, brightness); /* remember that in fade this has changed to analog because of the different levels of lighting available in analog */ 
+  brightness = brightness +fadeAmount; /* the light's brightness will start from 0 and the fade amount then will continue to add 5 to the current brightness level */
 
-  if (brightness <= 0 || brightness >= 255) {
+  if (brightness <= 0 || brightness >= 255) { /* conditiion, if brightness is fully on or off, the fade amount will decreade by the fade amount number */
     fadeAmount = -fadeAmount; 
     }
     
