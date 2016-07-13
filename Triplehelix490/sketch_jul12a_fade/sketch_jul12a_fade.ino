1ef58fbc33c580b7e19255bfd9a6385fa7c3ee0e
Triplehelix490 <adam.cellmer@yahoo.co.uk>
2016-07-13 11:10:11 +0100

    Fade

--- Triplehelix490/sketch_jul12a_fade/sketch_jul12a_fade.ino
+++ Triplehelix490/sketch_jul12a_fade/sketch_jul12a_fade.ino
@@ -1,24 +1,20 @@
-
+//    \/\/\/\/\/change these factors to change the characteristics of the components.
 int pin = 9;
 int brightness = 0;
-int fadeAmount = 255;
-int timeOn = 4000;
-int timeOff = 2000;
+int fadeAmount = 5;
+int timeOn = 400;
 
-//setup code: 
+//setup code: this pulls the program from the Arduino pre-program
 void setup() {
-    pinMode(pin, OUTPUT);
+  pinMode(pin, OUTPUT);
 }
-  
-//main code
 
+//main code: the main code which produce the desired effects for the components
 void loop (){
-
-    analogWrite(pin, HIGH);
-    brightness = brightness + fadeAmount
-      delay(timeOn);
-       analogWrite(pin, LOW);
-          delay(timeOff);
-
-
+      analogWrite(pin, brightness);
+        brightness = brightness + fadeAmount
+        if ( brightness <= 0 ||brightness >= 255) {
+        fadeAmount = - fadeAmount;
+}
+delay(timeOn);
 }
