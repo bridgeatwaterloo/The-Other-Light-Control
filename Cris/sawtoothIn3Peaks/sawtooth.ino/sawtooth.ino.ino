068cfaa4274f5eccc8c3e57fe388dd35476da84a
Cristina Gavril <cris.gavril@gmail.com>
2016-07-13 16:26:33 +0100

    sawtoothIn3PeaksWithIncreasingIntensity
    I have 3 peaks with different light intensity (85, 170, 255)
    The led should slowly light up to the 85 intensity, then turn off,
    slowly light to 170 then off, and slowly light to 255 and then off,
    after that the loop will repeat from the begining

---
+++ Cris/sawtoothIn3Peaks/sawtooth.ino/sawtooth.ino.ino
@@ -0,0 +1,32 @@
+int maxBrightness = 255;
+int sawtoothCount = 3; // how many peaks
+int sawtoothIncrease = maxBrightness / sawtoothCount; // 
+int stepDelay = 50; //ms to show one brightness
+int brightness = 0;
+int sawtoothPeak = sawtoothIncrease; // setting target to first peak
+int led = 11;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  if (brightness >= sawtoothPeak) { // at the peak
+    brightness = 0; // over the hump
+
+
+    // updating the peak target
+
+
+    if (sawtoothPeak < maxBrightness) {
+      sawtoothPeak += sawtoothIncrease; // increase peak
+    } else {
+      sawtoothPeak = sawtoothIncrease; // first peak
+    }
+  } else {
+    brightness += fadeAmount; // increasing brightness
+  }
+  
+  analogWrite(led, brightness);
+  delay(stepDelay);
+}
