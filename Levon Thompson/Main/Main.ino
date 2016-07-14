ad571b40497a35272061b2c2494db874fb926067
Levon2014 <lev.2014@outlook.com>
2016-07-14 10:58:21 +0100

    This is the newest updated version of my codes

--- Levon Thompson/Main/Main.ino
+++ Levon Thompson/Main/Main.ino
@@ -14,8 +14,17 @@ int pin = 9; //set pin to number 9 as a variable
 void setup() {
 	//put your setup code here, to run once:
 	pinMode(pin, OUTPUT);
+	turnLightOnThenOff();
 }
 
-void loop(){
-	fadeToALevel(150);
+void loop() {
+	// fadeToALevel(5);
+	for (int i=0; i < 6; i++){//
+      analogWrite(pin, 255);//on	
+      delay(1000);//amount of time light is on
+      analogWrite(pin, 0);//off
+      delay(100);//amount of time light is off
+	}
+	delay(1000);//time between each set of 6 flashes
 }
+
--- Levon Thompson/Main/lights.ino
+++ Levon Thompson/Main/lights.ino
@@ -5,3 +5,19 @@ void fadeToALevel(int fadeLevel) {
 
 
 }
+
+// Ali's function that turns the light on and off
+void turnLightOnThenOff(){
+	analogWrite(pin, 255); // set the pin to be on full
+	delay(1000);
+	analogWrite(pin, 0);
+
+}
+
+// Levon's function that turns the light on and off three times
+
+void turnLightOnandOff() {
+	analogWrite(pin, 250); // set the pin to be on full 
+	delay(150);
+	analogWrite(pin,0);
+    }
