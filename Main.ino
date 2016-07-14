7f38503c0fd756af91c047f4f992ae6f8c4273b8
mabatistussi <marisaba2012@gmail.com>
2016-07-14 10:15:29 +0100

    Add files via upload

---
+++ Main.ino
@@ -0,0 +1,17 @@
+/* This is the main file for the interactive light project. It will be uploaded to each Arduino.
+	The code that animates the lights is saved in separate dile called lights.h
+	The code that reads from the sensor is saved in a se[arate file ca]led sensors.h 
+*/
+int pin = 9; //variable value
+int count= 0;//counter starts with 0 value 
+
+void setup(){
+  pinMode(pin, OUTPUT);
+  }
+
+void threeFlashes(){}
+
+
+	     
+
+ 
